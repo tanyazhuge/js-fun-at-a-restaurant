2e9374d2df6d696058c3d61113dc234f1e8ae9b4
Tanya Zhuge <tangnazhuge@gmail.com>
2021-08-27 16:23:08 -0700

    Complete/update meal functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,54 @@
-function nameMenuItem() {
+function nameMenuItem(foodName) {
+  var descriptiveName = `Delicious ${foodName}`;
+  return descriptiveName;
+};
 
-}
+
+function createMenuItem(itemName, itemPrice, itemType) {
+  menuItem = {
+    name: itemName,
+    price: itemPrice,
+    type: itemType,
+  }
+return menuItem
+};
+
+
+var ingredients = [];
+function addIngredients(topping,ingredients){
+  if (ingredients.includes(topping) === false){
+    ingredients.push(topping);
+  }
+  return ingredients
+};
+
+
+function formatPrice(price){
+  return `$${price}`;
+};
+
+
+function decreasePrice(price){
+  var discountPrice = price * 0.9;
+  return discountPrice;
+};
+
+
+function createRecipe(title, toppings, itemType){
+  var recipe = {
+    title: title,
+    ingredients: toppings,
+    type: itemType,
+  }
+  return recipe;
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
