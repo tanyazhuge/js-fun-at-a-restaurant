6f5e32334c82f2d2ee8e2509484deca6d9912031
Tanya Zhuge <tangnazhuge@gmail.com>
2021-08-29 18:23:04 -0700

    Refactor and format order function

--- src/order.js
+++ src/order.js
@@ -1,35 +1,36 @@
-
 function takeOrder(orderName, deliveryOrders) {
-  if (deliveryOrders.length < 3){
+  if (deliveryOrders.length < 3) {
     deliveryOrders.push(orderName);
   }
-  return
+  return;
 };
 
 function refundOrder(orderNumber, deliveryOrders) {
-  for (var i = 0; i < deliveryOrders.length ;i++){
-    if(deliveryOrders[i].orderNumber === orderNumber){
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
       deliveryOrders.splice(i, 1);
     }
   }
   return deliveryOrders;
 };
 
-function listItems(deliveryOrders){
+function listItems(deliveryOrders) {
   var foodItems = ''
-  for (var i = 0; i < deliveryOrders.length; i++){
+  for (var i = 0; i < deliveryOrders.length; i++) {
     foodItems += deliveryOrders[i].item
-    if (i !== deliveryOrders.length-1){
+    if (i !== deliveryOrders.length - 1) {
       foodItems += ", ";
     }
   }
   return foodItems;
 };
 
+
+
 // do not want list of true or false, want to return only one
 function searchOrder(deliveryOrders, itemName) {
   var itemsIsPresent = []
-  for (var i = 0; i < deliveryOrders.length; i++){
+  for (var i = 0; i < deliveryOrders.length; i++) {
     itemsIsPresent.push(deliveryOrders[i].item.includes(itemName));
   }
   return itemsIsPresent.includes(true);
