61fe154d5895bb4ed70a5257f42ac3729bd394fd
Tanya Zhuge <tangnazhuge@gmail.com>
2021-08-29 18:16:02 -0700

    Complete Chef class

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,19 @@
 class Chef {
-  constructor() {
-    
+  constructor(chefName, restaurant) {
+    this.name = chefName;
+    this.restaurant = restaurant;
+
   }
-}
+  greetCustomer(customerName, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${customerName}!`;
+    } else {
+      return `Hello, ${customerName}!`;
+    }
+  }
+  checkForFood(foodItemInfo) {
+        return `Yes, we're serving ${foodItemInfo.name} today!`;
+  }
+};
 
 module.exports = Chef;
