53d1ca04dd12732267a1c7cc2dd8ae39b615e5d6
Tanya Zhuge <tangnazhuge@gmail.com>
2021-08-29 18:27:25 -0700

    Refactor and format restaurant function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,65 +1,53 @@
 function createRestaurant(restaurantName) {
- var newRestaurant = {
-   name: restaurantName,
-   menus: {
-     breakfast: [],
-     lunch: [],
-     dinner:[],
-   }
- }
- return newRestaurant;
+  var newRestaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+  return newRestaurant;
 };
 
-function addMenuItem (newRestaurant, foodItemInfo){
+function addMenuItem(newRestaurant, foodItemInfo) {
 
-// if (newRestaurant.menus.include){
-
-if (newRestaurant.menus.breakfast.includes(foodItemInfo)){
-    return;
-
-} else if (newRestaurant.menus.lunch.includes(foodItemInfo)){
-    return;
-
-} else if (newRestaurant.menus.dinner.includes(foodItemInfo)){
-    return;
-
-} else if ("breakfast" === foodItemInfo.type) {
+  if ("breakfast" === foodItemInfo.type && newRestaurant.menus.breakfast.includes(foodItemInfo) === false) {
     newRestaurant.menus.breakfast.push(foodItemInfo);
 
-} else if ("lunch" === foodItemInfo.type) {
+  } else if ("lunch" === foodItemInfo.type && newRestaurant.menus.lunch.includes(foodItemInfo) === false) {
     newRestaurant.menus.lunch.push(foodItemInfo);
 
-} else {
+  } else if ("dinner" === foodItemInfo.type && newRestaurant.menus.dinner.includes(foodItemInfo) === false) {
     newRestaurant.menus.dinner.push(foodItemInfo);
-}
+  }
 };
 
 
-function removeMenuItem(newRestaurant, foodItem, type){
-
-  if (type === "breakfast"){
-    for (var i = 0; i < newRestaurant.menus.breakfast.length; i++){
-      if(newRestaurant.menus.breakfast[i].name === foodItem){
+function removeMenuItem(newRestaurant, foodItem, type) {
+  if (type === "breakfast") {
+    for (var i = 0; i < newRestaurant.menus.breakfast.length; i++) {
+      if (newRestaurant.menus.breakfast[i].name === foodItem) {
         newRestaurant.menus.breakfast.splice(i);
       }
     }
 
   } else if (type === "lunch") {
-    for (var i = 0; i < newRestaurant.menus.lunch.length; i++){
-      if(newRestaurant.menus.lunch[i].name === foodItem){
+    for (var i = 0; i < newRestaurant.menus.lunch.length; i++) {
+      if (newRestaurant.menus.lunch[i].name === foodItem) {
         newRestaurant.menus.lunch.splice(i);
       }
     }
-  } else if (type === "dinner"){
-    for (var i = 0; i < newRestaurant.menus.dinner.length; i++){
-      if(newRestaurant.menus.dinner[i].name === foodItem){
+  } else if (type === "dinner") {
+    for (var i = 0; i < newRestaurant.menus.dinner.length; i++) {
+      if (newRestaurant.menus.dinner[i].name === foodItem) {
         newRestaurant.menus.dinner.splice(i);
       }
     }
-}
-  var errorItems = ["Mom's Spaghetti","Funfetti Cake",]
+  }
+  var errorItems = ["Mom's Spaghetti", "Funfetti Cake", ]
 
-  if (foodItem === errorItems[0] || foodItem === errorItems[1]){
+  if (foodItem === errorItems[0] || foodItem === errorItems[1]) {
     return `Sorry, we don't sell ${foodItem}, try adding a new recipe!`;
   } else {
     return `No one is eating our ${foodItem} - it has been removed from the ${type} menu!`;
